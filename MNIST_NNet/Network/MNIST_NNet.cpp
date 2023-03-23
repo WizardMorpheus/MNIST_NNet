f4e657ae505e64f469c7ed0a7f31f5e54219d1d9
Charlie Ridgeon <you@example.com>
2023-03-23 12:36:11 +1000

    changed from Vc to Eigen for Linear Algebra maths

--- MNIST_NNet/Network/MNIST_NNet.cpp
+++ MNIST_NNet/Network/MNIST_NNet.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+#include "../../eigen/Eigen/Dense"
+
 
 int main()
 {
+	Eigen::Vector3f v1(1, 0, 0);
+	Eigen::Vector3f v2(0, 1, 0);
 
+	Eigen::Vector3f v3 = v1.cross(v2);
+
+	std::cout << v1 << std::endl << std::endl << v2 << std::endl << std::endl << v3;
 }
