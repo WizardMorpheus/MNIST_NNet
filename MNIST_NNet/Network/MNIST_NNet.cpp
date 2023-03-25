f272dbbfd5499e6c71e4daa7b254e78cc9535565
Charlie Ridgeon <you@example.com>
2023-03-25 15:49:05 +1000

    fixed print command

--- MNIST_NNet/Network/MNIST_NNet.cpp
+++ MNIST_NNet/Network/MNIST_NNet.cpp
@@ -8,63 +8,75 @@
 
 using namespace std;
 
-#include "../../eigen/Eigen/Dense"
-
 vector<tuple<int, Eigen::VectorXf>> read_data(string file_name)
 {
     vector<tuple<int, Eigen::VectorXf>> ret;
 
     // File pointer
-    fstream fin;
+    ifstream fin;
 
-    // Open an existing file
-    fin.open(file_name, ios::in);
+    fin.open(file_name, ifstream::in);
+
+    bool check = fin.is_open();
 
     // Read the Data from the file
     // as String Vector
     string line, word, temp;
 
-    while (fin >> temp) {
+    Eigen::VectorXf row;
+    row.resize(784);
 
-        Eigen::VectorXf row;
 
-        // read an entire row and
-        // store it in a string variable 'line'
-        getline(fin, line);
+    // read every column data of a row and
+    // store it in a string variable, 'word'
 
-        // used for breaking words
+    while (getline(fin, line))
+    {
         stringstream s(line);
-
-        // read every column data of a row and
-        // store it in a string variable, 'word'
-
         getline(s, word, ',');
-
         int label = stoi(word);
 
-        while (getline(s, word, ',')) {
 
-            // add all the column data
-            // of a row to a vector
-            row.resize(row.size() + 1);
-            row(row.size() - 1) = stof(word);
+        for (int i = 0; i < 784; i++) {
+            getline(s, word, ',');
+            row(i) = stof(word);
         }
+
         ret.push_back(make_tuple(label, row));
     }
+    
+    return ret;
+}
+
+
+void print_image(tuple<int, Eigen::VectorXf>& image) 
+{
+    char shades[4] = { ' ', char(177), char(178), char(219) };
+
+    for (int i = 0; i < 4; i++) {
+        cout << shades[i];
+    }
+    cout << endl;
+
+    for (int i = 0; i < get<1>(image).size(); i++)
+    {
+        float num = (get<1>(image)(i));
+        int index = floor((num / 256) * 4);
+
+        if (i % 28 == 0) cout << endl;
+        cout << shades[index];
+    }
+    cout << endl;
 }
 
 
 int main()
 {
-	Eigen::Vector3f v1(1, 0, 0);
-	Eigen::Vector3f v2(0, 1, 0);
 
-	Eigen::Vector3f v3 = v1.cross(v2);
+    vector<tuple<int, Eigen::VectorXf>> train_data = read_data(".\\..\\MNIST_data\\mnist_train.txt");
+    vector<tuple<int, Eigen::VectorXf>> test_data = read_data(".\\..\\MNIST_data\\mnist_test.txt");
 
-	std::cout << v1 << std::endl << std::endl << v2 << std::endl << std::endl << v3;
+    print_image(test_data[100]);
 
-    vector<tuple<int, Eigen::VectorXf>> train_data = read_data("../../MNIST_Data/mnist_train.csv");
-    vector<tuple<int, Eigen::VectorXf>> test_data = read_data("../../MNIST_Data/mnist_test.csv");
 
-	std::cout << v1 << std::endl << std::endl << v2 << std::endl << std::endl << v3;
 }
