01e2972975b7c5c9ea3783b071992dd623b2080e
Charlie Ridgeon <you@example.com>
2023-03-27 11:34:22 +1000

    added Node and NeuralNet classes, Node has been implemented

--- MNIST_NNet/Network/MNIST_NNet.cpp
+++ MNIST_NNet/Network/MNIST_NNet.cpp
@@ -8,6 +8,101 @@
 
 using namespace std;
 
+vector<tuple<int, Eigen::VectorXf>> read_data(string file_name);
+
+void print_image(tuple<int, Eigen::VectorXf>& image);
+
+float ReLU(float x);
+
+float LeakyReLu(float x);
+
+class Node
+{
+public:
+    Node() {}
+
+    Node(float *Value, Eigen::VectorXf Weights, Eigen::VectorXf* Father_Layer, float (*Activation_function)(float))
+    {
+        _value = Value;
+        _weights = Weights;
+        _father_layer = Father_Layer;
+        _f = Activation_function;
+    }
+
+    ~Node() {}
+
+    void activate()
+    {
+        *_value = _f(_weights.dot(*_father_layer));
+    }
+
+    float get_value() {return *_value;}
+    Eigen::VectorXf get_weights() { return _weights; }
+    Eigen::VectorXf get_father_layer() { return *_father_layer; }
+    function<float(float)> get_activation_function() { return *_f; }
+
+
+private:
+    float* _value;
+    Eigen::VectorXf _weights;
+    Eigen::VectorXf* _father_layer;
+    float (*_f)(float);
+};
+
+class Deep_Net 
+{
+public:
+    Deep_Net()
+    {
+
+    }
+
+    ~Deep_Net() {}
+private:
+
+};
+
+
+
+int main()
+{
+
+    /*vector<tuple<int, Eigen::VectorXf>> train_data = read_data(".\\..\\MNIST_data\\mnist_train.txt");
+    vector<tuple<int, Eigen::VectorXf>> test_data = read_data(".\\..\\MNIST_data\\mnist_test.txt");
+
+    print_image(test_data[100]);*/
+    Eigen::VectorXf W;
+    Eigen::VectorXf FL;
+
+    W.resize(10);
+    FL.resize(10);
+    for (int i = 0; i < 10; i++)
+    {
+        W(i) = i;
+        FL(i) = i;
+    }
+
+    float Nf = float(0);
+
+    Node N(&Nf, W, &FL, &ReLU);
+
+    cout << W << endl;
+
+    cout << W.dot(FL) << endl;
+
+
+    cout << N.get_weights()(3) << " " << N.get_father_layer()(3)  << endl;
+
+    cout << N.get_value() << endl;
+    N.activate();
+    cout << N.get_value() << endl;
+
+
+
+}
+
+
+
 vector<tuple<int, Eigen::VectorXf>> read_data(string file_name)
 {
     vector<tuple<int, Eigen::VectorXf>> ret;
@@ -44,12 +139,11 @@ vector<tuple<int, Eigen::VectorXf>> read_data(string file_name)
 
         ret.push_back(make_tuple(label, row));
     }
-    
+
     return ret;
 }
 
-
-void print_image(tuple<int, Eigen::VectorXf>& image) 
+void print_image(tuple<int, Eigen::VectorXf>& image)
 {
     char shades[4] = { ' ', char(177), char(178), char(219) };
 
@@ -69,14 +163,12 @@ void print_image(tuple<int, Eigen::VectorXf>& image)
     cout << endl;
 }
 
-
-int main()
+float ReLU(float x)
 {
+    return max(float(0), x);
+}
 
-    vector<tuple<int, Eigen::VectorXf>> train_data = read_data(".\\..\\MNIST_data\\mnist_train.txt");
-    vector<tuple<int, Eigen::VectorXf>> test_data = read_data(".\\..\\MNIST_data\\mnist_test.txt");
-
-    print_image(test_data[100]);
-
-
+float LeakyReLu(float x) 
+{
+    return x > 0 ? x : 0.1*x;
 }
